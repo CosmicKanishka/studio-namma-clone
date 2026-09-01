e8b981cd11a1cdfedb5b57d3f9283cc1d67f350b
Kanishka Sharma <sharmakaniishka@gmail.com>
2026-09-01 12:47:55 +0530

    footer added

---
+++ footer.js
@@ -0,0 +1,95 @@
+let footer = document.createElement("footer");
+
+footer.id = "footer";
+
+footer.innerHTML = `
+
+    <div class="footerTop">
+
+        <div class="footerLeft">
+            <p>NEWS</p>
+            <p>STUDIO NAMMA</p>
+
+            <br>
+
+            <p>YOUTUBE</p>
+            <p>LINKEDIN</p>
+            <p>INSTAGRAM</p>
+            <p>LEGAL</p>
+            <p>SITE EN FRANÇAIS</p>
+        </div>
+
+
+        <div class="footerCenter">
+
+            <p>LET'S TALK!</p>
+
+            <p class="footerEmail">
+                HELLO@STUDIONAMMA.COM
+            </p>
+
+            <p>
+                COPYRIGHT 2025<br>
+                STUDIO NAMMA
+            </p>
+
+        </div>
+
+
+        <div class="footerRight">
+
+            <p>MENU</p>
+
+            <p>LET'S TALK!</p>
+
+        </div>
+
+    </div>
+
+
+    <div class="nammaContainer">
+
+        <h1 class="nammaText">NAMMA</h1>
+
+    </div>
+
+
+    <div class="footerBottom">
+
+        <p>WE CRAFT BOLD DESIGN & CLEAN WEBFLOW.</p>
+
+        <p>LOS ANGELES, CA</p>
+
+    </div>
+
+`;
+
+document.body.append(footer);
+
+
+/* NAMMA scroll animation */
+
+let nammaText = footer.querySelector(".nammaText");
+
+let nammaObserver = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            nammaText.classList.add("showNamma");
+
+        } else {
+
+            nammaText.classList.remove("showNamma");
+
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+
+nammaObserver.observe(nammaText);
